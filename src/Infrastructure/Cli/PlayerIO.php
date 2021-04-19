1fef52f7455bad49c19bcacbe726de2331372d14
Paweł Antosiak <pantosiak@gwo.pl>
2021-04-19 17:30:54 +0200

    Clear output after user input

--- src/Infrastructure/Cli/PlayerIO.php
+++ src/Infrastructure/Cli/PlayerIO.php
@@ -17,21 +17,29 @@ final class PlayerIO implements PlayerAction
 
     public function askForDecision(string $question, DecisionCollection $decisions): Decision
     {
+        $this->clearOutput();
         return $this->io->choice($question, $decisions->getItems());
     }
 
     public function askForInformation(string $question): string
     {
+        $this->clearOutput();
         return $this->io->ask($question);
     }
 
     public function askForConfirmation(string $question): bool
     {
+        $this->clearOutput();
         return $this->io->confirm($question);
     }
 
     public function note(string $message): void
     {
         $this->io->note($message);
+    }
+
+    private function clearOutput(): void
+    {
+        $this->io->write("\033\143");
     }
 }
