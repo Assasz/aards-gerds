24e8ff01ce5afd1f3242ad03c14f527e0f89a300
Assasz <contact@pawelantosiak.com>
2021-06-15 14:55:38 +0200

    Trade event implementation

--- src/Event/TradeEvent.php
+++ src/Event/TradeEvent.php
@@ -26,8 +26,38 @@ abstract class TradeEvent extends Event
     public function __invoke(Player $player, PlayerAction $playerAction): Decision
     {
         $playerAction->tell([(string) $this->context, '']);
-        $playerAction->listInventory($this->subject->getInventory());
+
+        $this->askForChoice($player, $playerAction);
 
         return $playerAction->askForDecision('What do you want to do next?', $this->decisionCollection);
+    }
+
+    private function askForChoice(Player $player, PlayerAction $playerAction): void
+    {
+        $choice = $playerAction->askForChoice('What do you want to do?', ['Buy', 'Sell']);
+
+        if ($choice === 'Buy') {
+            $playerAction->tell("{$this->subject->getName()}'s inventory:");
+            $playerAction->listInventory($this->subject->getInventory());
+
+            $choice = $playerAction->askForChoice(
+                'What do you want to buy?',
+                array_merge($this->subject->getInventory()->getItems(), ['Back']),
+            );
+        }
+
+        if ($choice === 'Sell') {
+            $playerAction->tell("Your inventory:");
+            $playerAction->listInventory($player->getInventory());
+
+            $choice = $playerAction->askForChoice(
+                'What do you want to sell?',
+                array_merge($player->getInventory()->getItems(), ['Back']),
+            );
+        }
+
+        if ($choice === 'Back') {
+            $this->askForChoice($player, $playerAction);
+        }
     }
 }
